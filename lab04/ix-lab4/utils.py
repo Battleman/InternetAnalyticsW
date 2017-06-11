b2e06d6c937588fdf1cbb93a95b8b60e82eb3130
Battleman <olivier.cloux@epfl.ch>
2017-06-11 12:14:01 +0200

    increased VSM efficiency ; modularized listPrettyPrint ; identified topics

--- lab04/ix-lab4/utils.py
+++ lab04/ix-lab4/utils.py
@@ -5,6 +5,23 @@
 
 import json, os, pickle
 
+def listPrettyPrint(l, n):
+    """Prints a list l on n columns to improve readability"""
+    if(n == 5):
+        for a,b,c,d,e in zip(l[::5],l[1::5],l[2::5],l[3::5],l[4::5]):
+            print('{:<22}{:<22}{:<22}{:<22}{:<}'.format(a,b,c,d,e))
+    if(n == 4):
+        for a,b,c,d in zip(l[::4],l[1::4],l[2::4],l[3::4]):
+            print('{:<30}{:<30}{:<30}{:<}'.format(a,b,c,d))
+    if(n == 3):
+        for a,b,c in zip(l[::3],l[1::3],l[2::3]):
+            print('{:<30}{:<30}{:<}'.format(a,b,c))
+    if(n == 2):
+        for a,b in zip(l[::2],l[1::2]):
+            print('{:<40}{:<}'.format(a,b))
+    if(len(l)%n != 0): #print remaining
+        for i in range(len(l)%n):
+            print(l[-(len(l)%n):][i], end='\t')
 
 def save_json(objects, path):
     """
